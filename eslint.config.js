7a7ae8e0157efc0f690f667bd0dd0a39c2119f9b
marcus <marcus.koch01@outlook.de>
2024-09-16 10:24:13 +0200

    Initial Commit - Basic Setup
    - Vite + TS
    - ESlint
    - Prettier

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import prettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default [
+  js.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.browser
+    }
+  },
+  {
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin
+    }
+  },
+  {
+    rules: {
+      ...tseslint.configs.recommended.rules, // Default rules
+      ...prettier.rules, // No conflicts between Prettier and Eslint
+      'prettier/prettier': 'error' // Prettier-Fehler als ESLint-Fehler anzeigen
+    }
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'build/',
+      'public/',
+      '.vscode/',
+      '.DS_Store',
+      '.env'
+    ]
+  } // Don't lint heres
+]
---
+++ readme.md
@@ -0,0 +1,56 @@
+# Frontend Challenge
+
+## Overview
+
+**Frontend Challenge** is an app that showcases a programmer's skills across various stages of development using modern tools like TypeScript, ESLint, Prettier, and Vite.
+
+## Project Details
+
+- **Author**: Marcus Koch
+- **Version**: `0.0.1`
+- **License**: Private
+
+## Scripts
+
+- **`dev`**: Start the development server.
+- **`build`**: Lint, compile, and build the project.
+- **`preview`**: Preview the production build.
+- **`lint`**: Run ESLint on the codebase.
+
+## Setup
+
+1. **Install dependencies**:
+
+   ```bash
+   pnpm install
+   ```
+
+2. **Start development server**:
+
+   ```bash
+   pnpm run dev
+   ```
+
+3. **Build the project**:
+
+   ```bash
+   pnpm run build
+   ```
+
+4. **Preview the build**:
+
+   ```bash
+   pnpm run preview
+   ```
+
+5. **Lint the code**:
+   ```bash
+   pnpm run lint
+   ```
+
+## Tools Used
+
+- **Vite**
+- **TypeScript**
+- **ESLint**
+- **Prettier**
