d7a97a56ba1148166d6fffa7b320d998969774a1
marcus k <marcus.koch01@outlook.de>
2024-09-21 11:43:54 +0300

    feat: Fetch Products from URL
    - Replace mock data with data from an url
    - Add async data handling

--- src/components/heroSlider/HeroSlider.ts
+++ src/components/heroSlider/HeroSlider.ts
@@ -1,9 +1,13 @@
 import './heroSlider.scss'
 import {
   ProductDetails,
-  products,
   Products as ProductsType
 } from '../../mock/heroSliderMockData'
+
+type ApiResponse = {
+  products: ProductsType
+  [key: string]: unknown
+}
 
 /**
  * @docType class
@@ -17,16 +21,16 @@ export default class HeroSlider extends HTMLElement {
   products: ProductsType = [] // Array to hold product details
   activeSliderNo: number = 0 // Tracks the current slide index
   activeSliderId: string | null = null // Clarify the indicator to highlight
-  slideLength: number = products.length // Total number of slides
+  slideLength: number = this.products.length // Total number of slides
   prevButton: HTMLButtonElement | null = null // Reference to the "Previous" button
   nextButton: HTMLButtonElement | null = null // Reference to the "Next" button
   sliderIndicators: HTMLElement | null = null // Reference to the slider indicators container
-
-  lowerBorder: number = 0 // Lower boundary for the indicators slice
-  upperBorder: number = 4 // Upper boundary for the indicators slice
+  url: string = ''
 
   // Constant for maximum number of indicators
   readonly MAX_INDICATORS: number = 4
+  lowerBorder: number = 0 // Lower boundary for the indicators slice
+  upperBorder: number = 4 // Upper boundary for the indicators slice
 
   private touchStartX: number = 0 // Start point of touch at x-axis
   private touchEndX: number = 0 // Endpoint of touch at x-axis
@@ -41,11 +45,17 @@ export default class HeroSlider extends HTMLElement {
     this.handleTouchEnd = this.handleTouchEnd.bind(this)
   }
 
+  // Specifies which attributes should be observed for changes.
+  static get observedAttributes(): string[] {
+    return ['url']
+  }
+
   // Called when the element is added to the DOM
-  connectedCallback() {
+  async connectedCallback() {
     // Initialize the activeSliderId with the first product's ID, if available
-    if (products.length > 0) {
-      this.activeSliderId = products[0]?.id.toString()
+    await this.fetchProducts()
+    if (this.products.length > 0) {
+      this.activeSliderId = this.products[0]?.id.toString()
     }
     this.render()
     this.setupEventListeners()
@@ -54,6 +64,23 @@ export default class HeroSlider extends HTMLElement {
   // Called when the element is removed from the DOM
   disconnectedCallback() {
     this.cleanupEventListeners()
+  }
+
+  // Called whenever one of the observed attributes is changed.
+  attributeChangedCallback(
+    name: string,
+    oldValue: string | null,
+    newValue: string | null
+  ) {
+    if (oldValue !== newValue) {
+      switch (name) {
+        case 'url':
+          this.url = newValue || ''
+          break
+      }
+
+      this.render()
+    }
   }
 
   // Set up event listeners into the DOM
@@ -99,6 +126,33 @@ export default class HeroSlider extends HTMLElement {
     }
   }
 
+  //Fetches product data from the specified URL.
+  async fetchProducts(): Promise<void> {
+    try {
+      const response = await fetch(this.url)
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch products: ${response.statusText}`)
+      }
+
+      const data: ApiResponse = await response.json()
+      this.products = data.products
+
+      this.slideLength = this.products.length
+      if (this.products.length > 0) {
+        this.activeSliderId = this.products[0].id.toString()
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error('Error fetching products:', error.message)
+        throw new Error(error.message)
+      } else {
+        console.error('Unknown error fetching products:', error)
+        throw new Error('Fetch error')
+      }
+    }
+  }
+
   handleTouchStart(event: TouchEvent) {
     this.touchStartX = event.changedTouches[0].screenX
   }
@@ -136,12 +190,12 @@ export default class HeroSlider extends HTMLElement {
     if (this.activeSliderNo === this.slideLength - 1) {
       // If on the last slide, loop back to the first slide
       this.activeSliderNo = 0
-      this.activeSliderId = products[0].id.toString()
+      this.activeSliderId = this.products[0].id.toString()
       this.lowerBorder = 0
       this.upperBorder = Math.min(this.MAX_INDICATORS, this.slideLength) // Show a maximum of 4 indicators
     } else {
       this.activeSliderNo++
-      this.activeSliderId = products[this.activeSliderNo].id.toString()
+      this.activeSliderId = this.products[this.activeSliderNo].id.toString()
 
       // Adjust indicator borders if necessary
       if (this.activeSliderNo >= this.upperBorder) {
@@ -160,15 +214,15 @@ export default class HeroSlider extends HTMLElement {
     if (this.activeSliderNo === 0) {
       // If at the first slide, loop to the last slide
       this.activeSliderNo = this.slideLength - 1
-      this.activeSliderId = products[this.activeSliderNo].id.toString()
+      this.activeSliderId = this.products[this.activeSliderNo].id.toString()
 
       // Show the last indicators based on the remaining slides
       this.lowerBorder =
-        this.slideLength - (products.length % this.MAX_INDICATORS)
+        this.slideLength - (this.products.length % this.MAX_INDICATORS)
       this.upperBorder = this.slideLength
     } else {
       this.activeSliderNo--
-      this.activeSliderId = products[this.activeSliderNo].id.toString()
+      this.activeSliderId = this.products[this.activeSliderNo].id.toString()
 
       // Adjust the indicator borders if necessary
       if (this.activeSliderNo < this.lowerBorder) {
@@ -183,13 +237,13 @@ export default class HeroSlider extends HTMLElement {
   // Show the selected slide based on its index
   showSelectSlide(index: number) {
     this.activeSliderNo = index
-    this.activeSliderId = products[index].id.toString()
+    this.activeSliderId = this.products[index].id.toString()
     this.render()
   }
 
   // Generate the HTML for the current slide's content
   updateHeroImage(): string {
-    const bgImage: string = products[this.activeSliderNo]?.images[0] ?? ''
+    const bgImage: string = this.products[this.activeSliderNo]?.images[0] ?? ''
 
     return `
       <div class="hero-slider__overlay">
@@ -198,9 +252,9 @@ export default class HeroSlider extends HTMLElement {
   }
   updateHeroText(): string {
     const headline: string =
-      products[this.activeSliderNo]?.title ?? 'Default Headline'
+      this.products[this.activeSliderNo]?.title ?? 'Default Headline'
     const description: string =
-      products[this.activeSliderNo]?.description ?? 'Default Description'
+      this.products[this.activeSliderNo]?.description ?? 'Default Description'
 
     return `
       <h1 class="hero-slider__headline">${headline}</h1>
@@ -210,9 +264,12 @@ export default class HeroSlider extends HTMLElement {
 
   // Generate the list of slide indicators
   indicatorList(): string {
-    return products
+    return this.products
       .slice(this.lowerBorder, this.upperBorder)
       .map((product: ProductDetails) => {
+        // const img = new Image()
+        // img.src = product.images[0]
+
         const isActive =
           this.activeSliderId === product.id.toString()
             ? 'hero-slider__slide-indicator--active'
